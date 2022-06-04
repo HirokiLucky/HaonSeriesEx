9e747132886861c3b0886ba7a2a79156ac3371b0
hiroki <nagadon0424@icloud.com>
2022-06-05 02:03:20 +0900

    update script    addForce

--- Assets/Scripts/UnityChanScript.cs
+++ Assets/Scripts/UnityChanScript.cs
@@ -1,18 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class UnityChanScript : MonoBehaviour
 {
+    private Rigidbody rb;
+    private float speed = 20.0f;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
+    //Update is called once per frame
     void Update()
     {
-        
+        float x = Input.GetAxis("Horizontal") * speed;
+        Vector3 v = new Vector3(x, 0, 0);
+        rb.AddForce(v);
     }
+
+    // private void FixedUpdate()
+    // {
+    //     
+    // }
 }
